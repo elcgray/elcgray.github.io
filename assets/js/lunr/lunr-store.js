59bde39f097f374ab12df9f6eca5b674dcbaa927
elcgray <elcgray@gmail.com>
2018-02-06 18:57:17 -0600

    Latest

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,151 @@
+var store = [{
+        "title": "Social Media and the Arab Uprisings",
+        "excerpt":"Social Media and the Arab Uprisings is the thesis of my Master of Science in Global Technology and Development. In it, I assess the relationship between social media and the revolutions of the Arab uprisings in Tunisia, Egypt, and Libya, and question whether that relationship is indicative of a broader...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/academics/arab-uprisings/",
+        "teaser":"http://localhost:4000/images/arab-uprisings.jpg"},{
+        "title": "Building Fences: Saudi Arabia's Strategy for National Security",
+        "excerpt":"Table of Contents Introduction al Qaeda Iraq ISIL Building Fences Conclusion References IntroductionThis month, Saudi Arabia announced the completion of the first phase of a multi-billion dollar plan to help protect itself from growing instability and unrest in neighboring countries. A 900-kilometer fence has been installed along the border of...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/academics/building-fences/",
+        "teaser":"http://localhost:4000/images/saudi-arabia.jpg"},{
+        "title": "Information Warfare: The Roles of Social Media in Conflict",
+        "excerpt":"Table of Contents Introduction Background Government Surveillance Conflict Deterrent Conflict Catalyst Relevance Strengths and Weaknesses Conclusion References IntroductionInformation warfare is not new. It has always held the power to incite or to assuage conflict. A well-timed release of scathing photos or information about an opponent could be enough to turn...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/academics/information-warfare/",
+        "teaser":"http://localhost:4000/images/nodal-network.jpg"},{
+        "title": "Never Again? The US Response to Genocide in Bosnia and Rwanda",
+        "excerpt":"Table of Contents Introduction Rwanda History United States Aid and Involvement Bosnia History United States Aid and Involvement Conclusion At Arm’s Length Media Influence Final Thoughts References IntroductionActions may speak louder than words, but sometimes words (or the lack thereof) are equally as poignant. The word “genocide” is one that...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/academics/never-again/",
+        "teaser":"http://localhost:4000/images/genocide.jpg"},{
+        "title": "Se Va a Poner Feo: The Culture of Violence in Mexico and Its Impact on Development Efforts",
+        "excerpt":"Table of Contents A Culture of Violence Mexico Today Organized Crime Police and Military Violence Vigilante Justice Public Reactions to Violence Government National and Social Media Development in Areas of Conflict Conclusion Reference A Culture of ViolenceIn 2011, the UNDP’s administrator, Helen Clark, spoke on the issue of pervasive violence...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/academics/se-va-a-poner-feo/",
+        "teaser":"http://localhost:4000/images/mexico.jpg"},{
+        "title": "Social Media and the Arab Uprisings: Introduction",
+        "excerpt":"Table of Contents “The people want the fall of the regime.” Social media’s role Why this matters “Arab Spring” References “The people want the fall of the regime.”Mohamed Bouazizi, a 26-year old Tunisian fruit vendor, set himself on fire in a final act of protest after being harassed and beaten...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/smau-introduction/",
+        "teaser":"http://localhost:4000/images/tahrir.jpg"},{
+        "title": "Social Media and the Arab Uprisings: Tunisia's Background",
+        "excerpt":"Table of Contents What triggered the movements in Tunisia? Corruption Repression Censorship Economic disparity How was the revolution accomplished? Al-sha‘b al-tunisi Organized movements Social media January 2011 Why this matters References What triggered the movements in Tunisia?Mohamed Bouazizi’s self immolation in late 2010 may have been the spark that ignited...","categories": [],
+        "tags": ["global","technology","development","social","media","conflict","society","technology","politics","arab uprisings","tunisia"],
+        "url": "http://localhost:4000/smau-background-tunisia/",
+        "teaser":"http://localhost:4000/images/tunisia.jpg"},{
+        "title": "Social Media and the Arab Uprisings: Egypt's Background",
+        "excerpt":"Table of Contents What triggered the movements in Egypt? Corruption Economic disparity Repression How was the revolution accomplished? Social media Organized movements February 2011 Why this matters References What triggered the movements in Egypt?The MENA region was watching as Tunisians ousted their autocratic regime. Egyptians undoubtedly saw similarities in their...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/smau-background-egypt/",
+        "teaser":"http://localhost:4000/images/egypt.jpg"},{
+        "title": "Social Media and the Arab Uprisings: Libya's Background",
+        "excerpt":"Table of Contents What triggered the movements in Libya? Volatility Corruption Repression How was the revolution accomplished? Unification International intervention October 2011 Why this matters References What triggered the movements in Libya?The background of Libya’s uprising is both simpler and more difficult to cover. It’s simple in that its history...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/smau-background-libya/",
+        "teaser":"http://localhost:4000/images/libya.jpg"},{
+        "title": "Social Media and the Arab Uprisings: Social Media in Tunisia",
+        "excerpt":"Table of Contents Youth Technology proliferation Al Jazeera Culture Why this matters References The Arab uprising’s accomplishments throughout the MENA region are often attributed to the use of social media. In comparison, social media was recognized as one of several common components in analysts’ assessments of Tunisia.YouthYouth initiated the uprisings...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/smau-analyses-tunisia/",
+        "teaser":"http://localhost:4000/images/tunisia-protest.jpg"},{
+        "title": "Social Media and the Arab Uprisings: Social Media in Egypt",
+        "excerpt":"Table of Contents Blogs Social networking Al Jazeera Culture Double-edged sword Why this matters References Many analysts have spoken confidently about the positive role that social media played in the Egyptian revolution of 2011, but potentially negative aspects of the relationship were addressed, as well. We’ll cover several common points...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/smau-analyses-egypt/",
+        "teaser":"http://localhost:4000/images/khaled-said.jpg"},{
+        "title": "Social Media and the Arab Uprisings: Social Media in Libya",
+        "excerpt":"Table of Contents Double-edged sword Social media Diaspora Journalists Why this matters References Libya’s uprisings began shortly after Mubarak was overthrown in Egypt. Like Mubarak, Gaddafi shut down Internet and mobile phone networks inside of Libya early on in the conflicts, signaling that he, like Mubarak, was aware of the...","categories": [],
+        "tags": ["global","technology","development","social","media","conflict","society","technology","politics","arab uprisings","libya"],
+        "url": "http://localhost:4000/smau-analyses-libya/",
+        "teaser":"http://localhost:4000/images/gaddafi-mural.jpg"},{
+        "title": "Social Media and the Arab Uprisings: Social Media in Tunisia Today",
+        "excerpt":"Table of Contents Facebook Twitter Internet and mobile access rates Internet freedom Why this matters References The Arab Social Media Report, issued by the Dubai School of Government, speaks favorably of social media’s current role in the region, noting that it is “already providing the medium for overcoming many of...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/smau-sm-today-tunisia/",
+        "teaser":"http://localhost:4000/images/twitter.jpg"},{
+        "title": "Social Media and the Arab Uprisings: Social Media in Egypt Today",
+        "excerpt":"Table of Contents Facebook Twitter Internet and mobile access rates Internet freedom Why this matters References Since Mubarak’s removal, Egyptians have struggled to find a leader supportive of their initial cause. Mohamed Morsi was elected in June of 2012 and was removed by Field Marshal Abdel Fattah el-Sisi in July...","categories": [],
+        "tags": ["global","technology","development","social","media","conflict","society","technology","politics","arab uprisings","egypt"],
+        "url": "http://localhost:4000/smau-sm-today-egypt/",
+        "teaser":"http://localhost:4000/images/facebook.jpg"},{
+        "title": "Social Media and the Arab Uprisings: Social Media in Libya Today",
+        "excerpt":"Table of Contents Facebook Twitter Internet and mobile access rates Internet freedom Why this matters References Libya has struggled to achieve peace and stability. After Gaddafi’s death, the country broke into factions based on tribes, regions, and even cities. In July of 2012, Libya’s first parliamentary elections were held and...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/smau-sm-today-libya/",
+        "teaser":"http://localhost:4000/images/free-libya.jpg"},{
+        "title": "Social Media and the Arab Uprisings: Tunisia's Status Today",
+        "excerpt":"Summary of events Year Month Events 2011 January Ben Ali is removed from rule, and an interim national government is established by Prime Minister Mohammed Ghannouchi.   February Ghannouchi resigns from his role as Prime Minister.   March A court order dissolves Ben Ali’s political party, Rally for Constitutional Democracy...","categories": [],
+        "tags": ["global","technology","development","social","media","conflict","society","technology","politics","arab uprisings","tunisia"],
+        "url": "http://localhost:4000/smau-tunisia-today/",
+        "teaser":"http://localhost:4000/images/lutte-finale.jpg"},{
+        "title": "Social Media and the Arab Uprisings: Egypt's Status Today",
+        "excerpt":"Summary of events Year Month Events 2011 February Mubarak is removed from rule, and power is assumed by the Supreme Council of the Armed Forces (SCAF).   March Constitutional reforms which allow new elections are approved.   April Tahrir Square fills with protestors, demonstrating against the slow pace of political...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/smau-egypt-today/",
+        "teaser":"http://localhost:4000/images/anti-scaf.jpg"},{
+        "title": "Social Media and the Arab Uprisings: Libya's Status Today",
+        "excerpt":"Summary of events Year Month Events 2011 October Gaddafi is killed. The National Transitional Congress (NTC) announces Libya’s liberation and plans to hold elections within eight months. 2012 January Former rebel forces clash in Benghazi, and Abdel Hafiz Ghoga, the deputy head of the NTC, resigns.   February Clashes between...","categories": [],
+        "tags": ["global","technology","development","social","media","conflict","society","technology","politics","arab uprisings","libya"],
+        "url": "http://localhost:4000/smau-libya-today/",
+        "teaser":"http://localhost:4000/images/election.jpg"},{
+        "title": "Social Media and the Arab Uprisings: Conclusion",
+        "excerpt":"Table of Contents Why this all matters References In his book, Writing on the wall: Social media, the first 2,000 years, Standage is quick to point out that “social media does not merely connect us to each other today—it also links us to the past” (Standage, 2013, p. 89). Protestors...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/smau-conclusion/",
+        "teaser":"http://localhost:4000/images/occupy.jpg"},{
+        "title": "Social Media and the Arab Uprisings: Analysis",
+        "excerpt":"This project is the culmination point of my Master of Science in Global Technology and Development at Arizona State University. As such, I’ve been asked to analyze what this all means in terms of social media and social movements beyond the Arab uprisings.Among its many uses, social media has acted...","categories": [],
+        "tags": ["global","technology","development","social","media","conflict","society","technology","politics","arab uprisings","tunisia","egypt","libya"],
+        "url": "http://localhost:4000/smau-analysis/",
+        "teaser":"http://localhost:4000/images/connected.jpg"},{
+        "title": "The Balkans: Introduction",
+        "excerpt":"Table of Contents Introduction Crash Course in Modern South Balkan History Yugoslavia World War II Josip Broz Tito Ethnic tension Economic downturn Death of Tito Slobodan Milošević The breakup of Yugoslavia Yugoslav Wars Further Reading Coming Soon IntroductionMy wife, Rachel, and I recently traveled to the Balkans. We intentionally chose...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/balkans/",
+        "teaser":"http://localhost:4000/images/sarajevo/sarajevo-pano.jpg"},{
+        "title": "The Balkans: Sarajevo",
+        "excerpt":"Table of Contents Welcome to Sarajevo Siege A little background Old City Walking tour Coming Soon Further Reading Welcome to SarajevoWe landed in Sarajevo in the evening and took in a view of the surrounding mountains at dusk while waiting to go through customs. Cigarette smoke wafted in the air....","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/bosnia-sarajevo/",
+        "teaser":"http://localhost:4000/images/sarajevo/sarajevo-park-pano.jpg"},{
+        "title": "The Balkans: Srebrenica",
+        "excerpt":"Table of Contents Into Republika Srpska Referendum day Srebrenica Return Trip Coming Soon Reading List Into Republika SrpskaBosnia has two autonomous entities: the Federation of Bosnia and Herzegovina (BiH) and Republika Srpska (RS). Sarajevo is within the Federation of BiH, and Srebrenica is in Republika Srpska. There are no border...","categories": [],
+        "tags": ["media","conflict","nationalism","politics","balkan","bosnia","srebrenica"],
+        "url": "http://localhost:4000/bosnia-srebrenica/",
+        "teaser":"http://localhost:4000/images/srebrenica/srebrenica-hill-pano.jpg"},{
+        "title": "The Balkans: Sarajevo to Dubrovnik",
+        "excerpt":"Table of Contents Goodbye, Sarajevo Konjić Jablanica Mostar Drive to Dubrovnik Coming Soon Reading List Goodbye, SarajevoOur taxi driver, Mili, picked us up in Sarajevo at 8AM. We headed west and then south with an end destination of Dubrovnik. Mili recommended several stops along the way so we could see...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/dubrovnik/",
+        "teaser":"http://localhost:4000/images/dubrovnik/croatian-coast-pano.jpg"},{
+        "title": "The Balkans: Korčula",
+        "excerpt":"Table of Contents Korčula Airbnb Cats Old city Maksimilijan garden Around the island Coming Soon KorčulaOur catamaran to Korčula wasn’t scheduled to depart until the early evening, so we walked around the Port of Dubrovnik, ate lunch at a cute restaurant on the bay, and then read by the hotel...","categories": [],
+        "tags": ["balkan","croatia","dubrovnik","korcula"],
+        "url": "http://localhost:4000/korcula/",
+        "teaser":"http://localhost:4000/images/korcula/bay-pano.jpg"},{
+        "title": "The Balkans: Split",
+        "excerpt":"SplitThe ferry ride to Split was full without being uncomfortable. Rachel and I sat toward the bow, which afforded us a nice view of port entries and the sunset. Our neighbors included a couple with an energetic and charming child around 2 years old, a New Zealander toward the end...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/split/",
+        "teaser":"http://localhost:4000/images/split/palace-wall.jpg"},{
+        "title": "The Balkans: Plitvice",
+        "excerpt":"Driving NorthWe drove through periods of rain and high winds as we left Split and headed through the mountains to Plitvice. Tunnels allowed us to cut through without too many sharp turns, though our little Opel Astra struggled to accelerate up the steeper inclines. We eventually left the highway for...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/plitvice/",
+        "teaser":"http://localhost:4000/images/plitvice/plitvice-mid-pano.jpg"},{
+        "title": "The Balkans: Zagreb",
+        "excerpt":"ZagrebRain followed us from Plitvice to Zagreb. We arrived late enough on a Sunday that we found a place to eat dinner, hung our wet clothes throughout the apartment in a vain attempt to dry them, were serenaded by a neighbor (listen below), and then went to bed.A neighbor singing...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/zagreb/",
+        "teaser":"http://localhost:4000/images/zagreb/zagreb-pano.jpg"},{
+        "title": "The Balkans: Slovenia",
+        "excerpt":"Table of Contents Slovenia Ljubljana Vintgar Gorge Lake Bled Hiša Franko Coming Soon SloveniaAside from a 30-minute stop at the Croatian-Slovenian border where passports and the train itself were inspected, we spent most of our time watching and listening to cities and countryside pass us by on our way to...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/slovenia/",
+        "teaser":"http://localhost:4000/images/slovenia/vintgar-pano.jpg"},{
+        "title": "Balkans Detour: Venice",
+        "excerpt":"VeniceWe took a day trip to Venice with a group of five other people in between our adventures in Slovenia. Ambroz, our guide from Roundabout travel picked us up at 8am. We parked around 11 and walked into the city via the Constitution Bridge over the Grand Canal.In essence, this...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/venice/",
+        "teaser":"http://localhost:4000/images/venice/venice-pano.jpg"}]
